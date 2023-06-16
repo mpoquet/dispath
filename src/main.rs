e5ee971ebc94496feebaf9b2caa214f2e01c34a4
Millian Poquet <millian.poquet@irit.fr>
2023-06-16 18:48:42 +0200

    code: enable --all-vars

--- src/main.rs
+++ src/main.rs
@@ -16,40 +16,65 @@ struct MainArgs {
     #[argh(option, short = 'r', default= "String::from(\".*\")")]
     regex: String,
 
-    /// entry separator. default=':'
-    #[argh(option, short = 's', default = "':'")]
-    sep: char,
-
     /// do not print the same entry twice (preserves entry order)
     #[argh(switch, short = 'u')]
     unique: bool,
+
+    /// display all set variables instead of vars
+    #[argh(switch, short = 'a')]
+    all_vars: bool,
+
+    /// entry separator. default=':'
+    #[argh(option, short = 's', default = "':'")]
+    sep: char,
 
     /// fail if vars are unset
     #[argh(switch)]
     fail_unset: bool,
 }
 
-fn main() -> Result<()> {
-    let args: MainArgs = argh::from_env();
-    let vars = match args.vars.as_slice() {
-        [] => vec![String::from("PATH")],
-        _ => args.vars,
-    };
-    let regex = Regex::new(&args.regex)?;
-    let mut var_contents = vec![];
+fn all_set_variables_values() -> Result<Vec<String>> {
+    let mut values = Vec::new();
+    for (key, value) in env::vars_os() {
+        match value.into_string() {
+            Ok(val) => values.push(val),
+            _ => match key.into_string() {
+                Ok(k) => bail!("could not read value of environment variable {k}"),
+                _ => bail!("could not read value of an environment variable whose key is unreadable too... fix your env!"),
+            }
+        }
+    }
+    Ok(values)
+}
 
-    // Read the content of the environment variables
+fn variable_values(vars: Vec<String>, fail_unset: bool) -> Result<Vec<String>> {
+    let mut values = vec![];
     for var_name in vars.iter() {
         match env::var(var_name) {
             Ok(var_content) => {
-                var_contents.push(var_content);
+                values.push(var_content);
             },
             Err(e) => match e {
-                std::env::VarError::NotPresent => if args.fail_unset { bail!("environment variable {var_name} is unset"); },
+                std::env::VarError::NotPresent => if fail_unset { bail!("environment variable {var_name} is unset"); },
                 _ => bail!("could not read value of environment variable {var_name}: {e}")
             }
         }
     }
+    Ok(values)
+}
+
+fn main() -> Result<()> {
+    let args: MainArgs = argh::from_env();
+    let regex = Regex::new(&args.regex)?;
+
+    // Read the content of the environment variables
+    let var_contents = match args.all_vars {
+        true => all_set_variables_values()?,
+        false => match args.vars.as_slice() {
+            [] => variable_values(vec![String::from("PATH")], args.fail_unset)?,
+            _ => variable_values(args.vars, args.fail_unset)?
+        }
+    };
 
     // Print the desired entries
     let entries = var_contents.iter()
