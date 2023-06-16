fd1039fdf89e8fb9b894d0f1fab89b5a9c21ff73
Millian Poquet <millian.poquet@irit.fr>
2023-06-16 18:13:01 +0200

    code: enable --unique

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 
 use argh::FromArgs;
 use anyhow::{Result, bail};
+use itertools::Itertools;
 use regex::Regex;
 
 #[derive(FromArgs)]
@@ -11,7 +12,7 @@ struct MainArgs {
     #[argh(positional)]
     vars: Vec<String>,
 
-    /// only matching entries are displayed. default=".*"
+    /// filter entries to display with a regex. default=".*"
     #[argh(option, short = 'r', default= "String::from(\".*\")")]
     regex: String,
 
@@ -19,17 +20,13 @@ struct MainArgs {
     #[argh(option, short = 's', default = "':'")]
     sep: char,
 
+    /// do not print the same entry twice (preserves entry order)
+    #[argh(switch, short = 'u')]
+    unique: bool,
+
     /// fail if vars are unset
     #[argh(switch)]
     fail_unset: bool,
-}
-
-fn display_path_value(content: String, sep: char, regex: &Regex) {
-    for entry in content.split(sep) {
-        if regex.is_match(&entry) {
-            println!("{entry}");
-        }
-    }
 }
 
 fn main() -> Result<()> {
@@ -39,16 +36,36 @@ fn main() -> Result<()> {
         _ => args.vars,
     };
     let regex = Regex::new(&args.regex)?;
+    let mut var_contents = vec![];
 
+    // Read the content of the environment variables
     for var_name in vars.iter() {
         match env::var(var_name) {
-            Ok(var_content) => display_path_value(var_content, args.sep, &regex),
+            Ok(var_content) => {
+                var_contents.push(var_content);
+            },
             Err(e) => match e {
                 std::env::VarError::NotPresent => if args.fail_unset { bail!("environment variable {var_name} is unset"); },
                 _ => bail!("could not read value of environment variable {var_name}: {e}")
             }
         }
     }
+
+    // Print the desired entries
+    let entries = var_contents.iter()
+        .flat_map(|c| c.split(args.sep))
+        .filter(|x| regex.is_match(&x));
+
+    if args.unique {
+        for entry in entries.unique() {
+            println!("{entry}");
+        }
+    } else {
+        for entry in entries {
+            println!("{entry}");
+        }
+    }
+
     Ok(())
 }
 
